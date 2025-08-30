bd02539fedbf7091c57242ec25a71a620d711c55
Arrhenius401 <1825270596@qq.com>
2025-08-30 14:14:16 +0800

    每日两题

---
+++ src/GreedAlgorithm/problem3.java
@@ -0,0 +1,31 @@
+package GreedAlgorithm;
+
+public class problem3 {
+    public int jump(int[] nums) {
+        int max = 0, cur = 0, count = 0, lastMax = 0;
+
+        while (cur < nums.length && cur <= max) {
+            if(max >= nums.length-1){
+                break;
+            }
+
+            for(int i=cur; i<=lastMax; i++){
+                if (max < i + nums[i]) {
+                    max = i + nums[i];
+                }
+            }
+            cur = lastMax+1;
+            lastMax = max;
+            count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,1,1,1,1};
+        problem3 p = new problem3();
+
+        System.out.println(p.jump(nums));
+    }
+}
