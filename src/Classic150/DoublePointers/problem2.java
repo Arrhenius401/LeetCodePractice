a211b7bf29748d1ebe477c1767a1acd254ef623d
Arrhenius401 <1825270596@qq.com>
2025-10-15 16:08:18 +0800

    每日一题

---
+++ src/Classic150/DoublePointers/problem2.java
@@ -0,0 +1,27 @@
+package Classic150.DoublePointers;
+
+//给你一个下标从 1 开始的整数数组 numbers ，该数组已按 非递减顺序排列  ，请你从数组中找出满足相加之和等于目标数 target 的两个数。如果设这两个数分别是 numbers[index1] 和 numbers[index2] ，则 1 <= index1 < index2 <= numbers.length 。
+//
+//以长度为 2 的整数数组 [index1, index2] 的形式返回这两个整数的下标 index1 和 index2。
+//
+//你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
+//
+//你所设计的解决方案必须只使用常量级的额外空间。
+
+public class problem2 {
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0, end = numbers.length-1;
+
+        while (start < end){
+            if(numbers[start]+numbers[end] > target){
+                end--;
+            } else if (numbers[start]+numbers[end] < target){
+                start++;
+            } else {
+                return new int[]{start, end};
+            }
+        }
+
+        return new int[]{-1,-1};
+    }
+}
