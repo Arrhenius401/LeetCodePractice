fd43f50586564eff313f2c616639efdef95b700e
Arrhenius401 <1825270596@qq.com>
2025-12-02 14:26:42 +0800

    每日一题

---
+++ src/Classic150/DynamicProgramPlus/problem5.java
@@ -0,0 +1,23 @@
+package Classic150.DynamicProgramPlus;
+
+//给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+//
+//设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+//
+//注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+public class problem5 {
+    public int maxProfit(int[] prices) {
+        int buy1 = -prices[0], sell1 = 0;
+        int buy2 = -prices[0], sell2 = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            buy1 = Math.max(buy1, -prices[i]);
+            sell1 = Math.max(sell1, prices[i] + buy1);
+            buy2 = Math.max(buy2, sell1 - prices[i]);
+            sell2 = Math.max(sell2, prices[i] + buy2);
+        }
+
+        return Math.max(0, Math.max(sell1, sell2)); //其实最终是sell2
+    }
+}
