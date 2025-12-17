8d7e2943e291d7178696f5168212c344ec5a5445
Arrhenius401 <1825270596@qq.com>
2025-12-17 11:01:27 +0800

    SQL每日两题

---
+++ src/Sql50/AdvancedStringFunctions/problem2.sql
@@ -0,0 +1,21 @@
+-- 患者信息表： Patients
+--
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | patient_id   | int     |
+-- | patient_name | varchar |
+-- | conditions   | varchar |
+-- +--------------+---------+
+-- 在 SQL 中，patient_id （患者 ID）是该表的主键。
+-- 'conditions' （疾病）包含 0 个或以上的疾病代码，以空格分隔。
+-- 这个表包含医院中患者的信息。
+--
+--
+-- 查询患有 I 类糖尿病的患者 ID （patient_id）、患者姓名（patient_name）以及其患有的所有疾病代码（conditions）。I 类糖尿病的代码总是包含前缀 DIAB1 。
+--
+-- 按 任意顺序 返回结果表。
+
+SELECT *
+FROM Patients
+WHERE conditions like 'DIAB1%' OR conditions like '% DIAB1%';
